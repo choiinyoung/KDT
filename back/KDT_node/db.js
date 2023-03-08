ec9df1e85761abcc8d3ac898cedf23525925d260
choiinyoung <choi121100@naver.com>
2023-03-08 10:21:27 +0900

    callback

---
+++ back/KDT_node/db.js
@@ -0,0 +1,30 @@
+// function func1(callback) {
+//   console.log('1번 함수');
+//   function fakefuntion3(){
+//     console.log('3번 인 척 하는 함수');
+//   }
+  
+//   callback(fakefuntion3);
+// }
+// function func2(callback) {
+//   console.log('2번 함수');
+//   callback();
+// }
+// function func3() {
+//   console.log('3번 함수');
+// }
+// func1(() => {
+//   console.log('2번 인척하는 새로 만든 익명 함수');
+// });
+
+function multiplication(number, callback) {
+  let answer = 0;
+  setTimeout(function () {
+  answer = number * number;
+  callback(answer);
+  }, 2000);
+}
+function say(result) {
+  console.log(result);
+}
+multiplication(3, say);
