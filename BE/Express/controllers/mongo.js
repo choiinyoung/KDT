17b4a308ca54a324610561d1da83eb2508196ff3
choiinyoung <choi121100@naver.com>
2023-03-21 11:13:23 +0900

    mongo DB 쿼리공부

--- BE/Express/controllers/mongo.js
+++ BE/Express/controllers/mongo.js
@@ -6,24 +6,220 @@ const client = new MongoClient(uri, {
   useUnifiedTopology: true,
   serverApi: ServerApiVersion.v1,
 });
+
+// client.connect((err) => {
+//   const test = client.db('kdt5').collection('test');
+
+//   test.deleteMany({}, (deleteErr, deleteResult) => {
+//     if (deleteErr) throw deleteErr;
+//     console.log(deleteResult);
+
+//     test.insertOne(
+//       {
+//         name: 'pororo',
+//         age: 5,
+//       },
+//       (insertErr, insertResult) => {
+//         if (insertErr) throw insertErr;
+//         console.log(insertResult);
+//       },
+//     );
+//   });
+//   // perform actions on the collection object
+//   // test.deleteMany({}, (deleteErr, deleteResult) => {
+//   //   if (deleteErr) throw deleteErr;
+//   //   console.log(deleteResult);
+//   //   test.insertOne(
+//   //     {
+//   //       name: 'young',
+//   //       nickName: 'young22',
+//   //     },
+//   //     (insertErr, insertResult) => {
+//   //       console.log(insertResult);
+//   //       // client.close();
+//   //       const findCursor = test.find({});
+//   //       findCursor.toArray((err, data) => {
+//   //         console.log(data);
+//   //       });
+//   //     },
+//   //   );
+//   // });
+// });
+
+// // insertMany
+// client.connect((err) => {
+//   const test = client.db('kdt5').collection('test');
+
+//   test.deleteMany({}, (deleteErr, deleteResult) => {
+//     if (deleteErr) throw deleteErr;
+//     console.log(deleteResult);
+
+//     test.insertMany(
+//       [
+//         { name: 'pororo', age: 5 },
+//         { name: 'loopy', age: 6 },
+//         { name: 'crong', age: 4 },
+//       ],
+//       (insertErr, insertResult) => {
+//         if (insertErr) throw insertErr;
+//         console.log(insertResult);
+//       },
+//     );
+//   });
+// });
+
+// deleteOne 쿼리
+// client.connect((err) => {
+//   const test = client.db('kdt5').collection('test');
+
+//   test.deleteMany({}, (deleteErr, deleteResult) => {
+//     if (deleteErr) throw deleteErr;
+//     console.log(deleteResult);
+
+//     test.insertMany(
+//       [
+//         { name: 'pororo', age: 5 },
+//         { name: 'loopy', age: 6 },
+//         { name: 'crong', age: 4 },
+//       ],
+//       (insertErr, insertResult) => {
+//         if (insertErr) throw insertErr;
+//         console.log(insertResult);
+
+//         test.deleteOne({ name: 'crong' }, (deleteOneErr, deleteOneResult) => {
+//           if (deleteOneErr) throw deleteOneErr;
+//           console.log(deleteOneResult);
+//         });
+
+//         test.deleteMany({ age: 5 }, (deleteManyErr, deleteManyResult) => {
+//           if (deleteManyErr) throw deleteManyErr;
+//           console.log(deleteManyResult);
+//         });
+//         // client.close();
+//       },
+//     );
+//   });
+// });
+
+// deleteMany 쿼리
+// client.connect((err) => {
+//   const test = client.db('kdt5').collection('test');
+
+//   test.deleteMany({}, (deleteErr, deleteResult) => {
+//     if (deleteErr) throw deleteErr;
+//     console.log(deleteResult);
+
+//     test.insertMany(
+//       [
+//         { name: 'pororo', age: 5 },
+//         { name: 'loopy', age: 6 },
+//         { name: 'crong', age: 4 },
+//       ],
+//       (insertErr, insertResult) => {
+//         if (insertErr) throw insertErr;
+//         console.log(insertResult);
+
+//         test.deleteMany(
+//           { age: { $gte: 5 } },
+//           (deleteManyErr, deleteManyResult) => {
+//             if (deleteManyErr) throw deleteManyErr;
+//             console.log(deleteManyResult);
+//           },
+//         );
+//         // client.close();
+//       },
+//     );
+//   });
+// });
+
+// update 쿼리
+// client.connect((err) => {
+//   const test = client.db('kdt5').collection('test');
+//   test.deleteMany({}, (deleteErr, deleteResult) => {
+//     if (deleteErr) throw deleteErr;
+//     test.insertMany(
+//       [
+//         { name: 'pororo', age: 5 },
+//         { name: 'loopy', age: 6 },
+//         { name: 'crong', age: 4 },
+//       ],
+//       (insertErr, insertResult) => {
+//         if (insertErr) throw insertErr;
+//         test.updateOne(
+//           { name: 'loopy' },
+//           { $set: { name: '루피' } },
+//           (updateErr, updateResult) => {
+//             if (updateErr) throw updateErr;
+//             console.log(updateResult);
+//           },
+//         );
+//       },
+//     );
+//   });
+// });
+
+// updateMany
+// client.connect((err) => {
+//   const test = client.db('kdt5').collection('test');
+//   test.deleteMany({}, (deleteErr, deleteResult) => {
+//     if (deleteErr) throw deleteErr;
+//     test.insertMany(
+//       [
+//         { name: 'pororo', age: 5 },
+//         { name: 'loopy', age: 6 },
+//         { name: 'crong', age: 4 },
+//       ],
+//       (insertErr, insertResult) => {
+//         if (insertErr) throw insertErr;
+//         test.updateMany(
+//           { age: { $gte: 5 } },
+//           { $set: { name: '5살 이상인 친구들' } },
+//           (updateErr, updateResult) => {
+//             if (updateErr) throw updateErr;
+//             console.log(updateResult);
+//           },
+//         );
+//       },
+//     );
+//   });
+// });
+
+// findOne
 client.connect((err) => {
   const test = client.db('kdt5').collection('test');
-  // perform actions on the collection object
   test.deleteMany({}, (deleteErr, deleteResult) => {
     if (deleteErr) throw deleteErr;
-    console.log(deleteResult);
-    test.insertOne(
-      {
-        name: 'young',
-        nickName: 'young22',
-      },
+    test.insertMany(
+      [
+        { name: 'pororo', age: 5 },
+        { name: 'loopy', age: 6 },
+        { name: 'crong', age: 4 },
+      ],
       (insertErr, insertResult) => {
-        console.log(insertResult);
-        // client.close();
-        const findCursor = test.find({});
-        findCursor.toArray((err, data) => {
-          console.log(data);
+        if (insertErr) throw insertErr;
+        test.findOne({ name: 'loopy' }, (findErr, findData) => {
+          console.log(findData);
         });
+      },
+    );
+  });
+});
+
+// find
+client.connect((err) => {
+  const test = client.db('kdt5').collection('test');
+  test.deleteMany({}, (deleteErr, deleteResult) => {
+    if (deleteErr) throw deleteErr;
+    test.insertMany(
+      [
+        { name: 'pororo', age: 5 },
+        { name: 'loopy', age: 6 },
+        { name: 'crong', age: 4 },
+      ],
+      (insertErr, insertResult) => {
+        if (insertErr) throw insertErr;
+        const findCursor = test.find({ name: 'loopy' });
+        console.log(findCursor);
       },
     );
   });
